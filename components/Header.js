4174f6fcf3ff546d94d0c0e53b1ed82bdc2a6ccc
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-08-14 11:36:42 -0400

    created a styled component for logo

--- components/Header.js
+++ components/Header.js
@@ -5,15 +5,19 @@ import { styled } from 'styled-components'
 // Utilized Styled Components documentation for syntax, add our css inside the variable
 const StyledHeader = styled.header`
   background-color: #4B9CD3;
-
-
 `
 
+
+// Styled Component for Logo LINK
+const StyledLogo = styled(Link)`
+  color:white;
+  text-decoration: none;
+`
 
 const Header = () => {
   return (
     <StyledHeader>
-        <Link href={'/'}>Ecommerce</Link>
+        <StyledLogo href={'/'}>Best Taekwondo Online</StyledLogo>
         <nav> 
             <Link href={'/'}>Home</Link>
             <Link href={'/products'}>All Products</Link>
