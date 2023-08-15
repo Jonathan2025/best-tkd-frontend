e041b2d119f45c7459464fd97289021aef5976cb
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-08-15 12:29:37 -0400

    added the products model from admin panel

---
+++ models/Product.js
@@ -0,0 +1,19 @@
+// This will be our products model schema 
+// Copied from our admin panel 
+
+import mongoose, { model, Schema, models } from "mongoose"
+
+// Mongoose Schema is the blue print that defines the structure of documents that will be stored in MongoDB
+const ProductSchema = new Schema({
+    title: {type:String, required:true}, 
+    description: String, 
+    price: {type:Number, required:true},
+    images: [{type:String}],
+    category: {type:mongoose.Types.ObjectId, ref:'Category'}, // reference refers to what we are adding, which is the category model
+    properties: {type:Object}
+})
+
+// After creating a schema, we created a mongoose model based on that schema 
+const Product = models.Product || model('Product', ProductSchema) // here if the product model doesnt exist then create a new mongoose model
+
+export default Product
