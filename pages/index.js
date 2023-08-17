8b2590bed4aa7268c0d0034bdb27474ecf6664d7
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-08-16 20:13:03 -0400

    able to get the featured product information from the server side

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 
 import Header from '@/components/Header'
 import Featured from '@/components/Featured'
+import Product from '@/models/Product'
+import mongooseConnect from '@/lib/mongoose'
 
-const index = () => {
+// Pass in the product as a prop
+const index = ({product}) => {
+
+  console.log('here is the product', product)
   return (
     <div>
       <Header/>
@@ -12,3 +17,19 @@ const index = () => {
 }
 
 export default index
+
+// Essentially we got the id of the featured product from the admin side and then now we are making a mongoose connection to it
+// Get server side props returns JSON that will be used to render the page because we then pass it into the index above as props
+export async function getServerSideProps() {
+  const featuredProductId = '64dd510728f31c38f84bea06'
+  await mongooseConnect()
+  const product = await Product.findById(featuredProductId) 
+  return {
+    // because mongoose objects cant be used by json, we need to stringify it and then parse it so that its not a string
+    props: {
+      product: JSON.parse(JSON.stringify(product))
+    
+    
+    },
+  }
+}
