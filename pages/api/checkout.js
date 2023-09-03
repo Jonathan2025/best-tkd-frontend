d177da7c8b8cd5539bae62a2648dfd99d6f9005a
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-09-03 14:18:13 -0400

    able to put items into cartItems to be sent to stripe api later

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,6 +1,8 @@
+import mongooseConnect from '@/lib/mongoose'
+import Product from '@/models/Product'
 import React from 'react'
 
-const checkoutHandler = (req,res) => {
+const checkoutHandler = async(req,res) => {
     // When the user checks out, it should be a POST request
     if(req.method !== 'POST'){
         res.json('Needs to be a post request')
@@ -11,9 +13,42 @@ const checkoutHandler = (req,res) => {
     // Otherwise we should be able to get the user information from req.body
     // remember products is the array of product ids in the cart
     const {name, email, city, zip, address, country, products} = req.body
+
+
+    await mongooseConnect() // import mongoose so that we can then access the products model 
+
+
+
+
     const productIds = products.split(',') // these are all the ids including duplicates 
-    const uniqueIds = [...newSet(productIds)]// using set will get the unique ids
+    const uniqueIds = [...new Set(productIds)]// using set will get the unique ids
+    const productInfos = await Product.find({_id:uniqueIds}) // so here we want to look through products for the products in the cart 
     
+    // Now we want to prepare to send the products to Strip API 
+    // pretty much we check the cart to see if we have things in the cart then we push it into the cartItems list
+    let cartItems = []
+    for (const productId of uniqueIds){
+        const productInfo = productInfos.find(product => product._id.toString() === productId)
+        const quantity = productIds.filter(id => id === productId)?.length || 0 // the id should be the same as the productId
+        
+        if (quantity > 0 && productInfo){
+            cartItems.push({
+                quantity, 
+                price_data: {
+                    currency: 'USD',
+                    product_data: {name: productInfo.title}, 
+                    unit_amount: quantity * productInfo.price,
+                }
+
+
+            })
+        }
+
+    }
+
+
+
+    res.json({cartItems})
 }
 
 export default checkoutHandler
